a8dd2661c7856290bb577177f1c166462851504e
Sudarshan <sudarshand.jadhav@gmail.com>
2021-08-23 23:56:57 +0530

    Styled the team Page

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -2,6 +2,9 @@ import {React, useEffect , useState} from 'react';
 import {useParams} from 'react-router-dom';
 import {MatchDetailCard} from "../componets/MatchDetailCard";
 import {MatchSmallCard} from "../componets/MatchSmallCard";
+import './TeamPage.scss';
+import { PieChart } from 'react-minimal-pie-chart';
+
 
 export const TeamPage = () => {
 
@@ -23,11 +26,30 @@ export const TeamPage = () => {
     }
     return (
         <div className="TeamPage">
-            <h1>{team.teamName}</h1>
-            <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
-            {team.matches.slice(1).map(match =>
+            <div className="team-name-section">
+                <h1 className="team-name">{team.teamName}</h1>
+            </div>
+            <div className="win-loss-section">
+                Wins / Losses
+                <PieChart
+                    data={[
+                        { title: 'Loss', value: team.totalMatches - team.totalWins, color: '#a34d5d' },
+                        { title: 'Wins', value: team.totalWins, color: '#4da375' }
+
+                    ]}
+                />
+            </div>
+            <div className="match-detail-section">
+                <h3>Latest Matches</h3>
+                <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
+            </div>
+            {
+                team.matches.slice(1).map(match =>
                 <MatchSmallCard key={match.id} teamName={team.teamName} match = {match}/>)
             }
+            <div className="more-link">
+                <a href="#">More ></a>
+            </div>
         </div>
     );
 }
